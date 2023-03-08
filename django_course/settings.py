6c43c83b00f76aa3717cf6ab19f69dc201346ac0
Fahad Alothman <dev.fahad.alothman@gmail.com>
2023-03-08 20:14:16 +0300

    complete module 5

--- django_course/settings.py
+++ django_course/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "core",
+    "core",  # core app
+    "users",  # users app
+    "django_extensions",  # extensions library
 ]
 
 MIDDLEWARE = [
